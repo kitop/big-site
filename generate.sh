41157696b4510297f0576f844826ec37b658233b
Esteban Pastorino <ejpastorino@gmail.com>
2018-08-09 20:37:22 +0200

    make directory depth more dynamic

--- generate.sh
+++ generate.sh
@@ -5,7 +5,8 @@ mkdir -p site
 for n in {1..1500}; do
   prefix=""
   if [ $((RANDOM % 2)) -eq 0 ]; then
-    prefix=$(echo $RANDOM | sed 's/\([0-9]\{0,2\}\)/\1\//g')
+    s=$((RANDOM % 10 + 1))
+    prefix=$(echo $RANDOM$RANDOM | sed "s/\\([0-9]\\{0,$s\\}\\)/\\1\\//g")
     mkdir -p site/${prefix}
   fi
   dd if=/dev/urandom of=site/${prefix}$( printf %03d "$n" ).html bs=1 count=$(( (RANDOM + 1024) / 4)) 2>&1 | grep "records out"
