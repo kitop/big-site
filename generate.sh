b278629470f48b5df2be40b4b9194c5c271b6fc0
Esteban Pastorino <ejpastorino@gmail.com>
2018-08-09 20:23:54 +0200

    smaller files

--- generate.sh
+++ generate.sh
@@ -8,5 +8,5 @@ for n in {1..1500}; do
     prefix=$(echo RANDOM | sed 's/\([0-9]\{0,2\}\)/\1\//g')
     mkdir -p site/${prefix}
   fi
-  dd if=/dev/urandom of=site/${prefix}$( printf %03d "$n" ).html bs=1 count=$(( (RANDOM + 1024) / 2)) 2>&1 | grep "records out"
+  dd if=/dev/urandom of=site/${prefix}$( printf %03d "$n" ).html bs=1 count=$(( (RANDOM + 1024) / 4)) 2>&1 | grep "records out"
 done
