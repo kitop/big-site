ad087454b611dce35fdc5bc8c1bb8b78a8f762b5
Esteban Pastorino <ejpastorino@gmail.com>
2018-08-09 20:27:12 +0200

    fix $RANDOM

--- generate.sh
+++ generate.sh
@@ -5,7 +5,7 @@ mkdir -p site
 for n in {1..1500}; do
   prefix=""
   if [ $((RANDOM % 2)) -eq 0 ]; then
-    prefix=$(echo RANDOM | sed 's/\([0-9]\{0,2\}\)/\1\//g')
+    prefix=$(echo $RANDOM | sed 's/\([0-9]\{0,2\}\)/\1\//g')
     mkdir -p site/${prefix}
   fi
   dd if=/dev/urandom of=site/${prefix}$( printf %03d "$n" ).html bs=1 count=$(( (RANDOM + 1024) / 4)) 2>&1 | grep "records out"
