1bbb5789729e9b513c57728179c825f01dde29b5
Esteban Pastorino <ejpastorino@gmail.com>
2018-08-09 20:21:06 +0200

    Generate with some subdirectories

--- generate.sh
+++ generate.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
-mkdir site
+mkdir -p site
 
-for n in {1..100}; do
-  dd if=/dev/urandom of=site/$( printf %03d "$n" ).html bs=1 count=$(( RANDOM + 1024 ))
+for n in {1..1500}; do
+  prefix=""
+  if [ $((RANDOM % 2)) -eq 0 ]; then
+    prefix=$(echo RANDOM | sed 's/\([0-9]\{0,2\}\)/\1\//g')
+    mkdir -p site/${prefix}
+  fi
+  dd if=/dev/urandom of=site/${prefix}$( printf %03d "$n" ).html bs=1 count=$(( (RANDOM + 1024) / 2)) 2>&1 | grep "records out"
 done
