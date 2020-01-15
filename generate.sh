a62a47a7d07281b841ac61829de6187e0aa6225c
Esteban Pastorino <ejpastorino@gmail.com>
2020-01-15 17:34:16 +0000

    allow to tweaak with env variables

--- generate.sh
+++ generate.sh
@@ -2,7 +2,17 @@
 
 mkdir -p site
 
-for n in {1..7000}; do
+if [ -z "$NUM_FILES" ]; then
+  echo "No NUM_FILES set, using default of 500"
+  NUM_FILES=500
+fi
+
+if [ -z "$FILE_SIZE" ]; then
+  echo "No FILE_SIZEset, using default of 1024"
+  FILE_SIZE=1024
+fi
+
+for n in $(seq ${NUM_FILES}); do
   prefix=""
   if [ $((RANDOM % 2)) -eq 0 ]; then
     s=$((RANDOM % 10 + 1))
@@ -12,5 +22,5 @@ for n in {1..7000}; do
   if [ $(($n % 100)) -eq 0 ]; then
     echo "$n pages generated"
   fi
-  dd if=/dev/urandom of=site/${prefix}$( printf %03d "$n" ).txt bs=1 count=$(( 10 * 1024)) > /dev/null 2> /dev/null
+  dd if=/dev/urandom of=site/${prefix}$( printf %03d "$n" ).txt bs=1 count=${FILE_SIZE} > /dev/null 2> /dev/null
 done
